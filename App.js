252e662e6912e7b28937a5a5ca4ee0e6bbe9ab57
Gonçalo Pinto <goncalopintoneto@gmail.com>
2023-03-27 23:31:54 +0100

    Native Todo
    Organize your way and day, with Native Todo.

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks 😀</Text>
+
+        <View style={styles.items}>
+          {/* This is where the Rasks will go! */}
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)} >
+                  <Task text={item} />
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+
+      </View>
+
+      {/* Write a Task */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder="Write a task" value={task} onChangeText={text => setTask(text)} />
+
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -13,8 +60,75 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 16.00,
+
+    elevation: 24,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    width: 250,
+    fontWeight: '500',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 16.00,
+
+    elevation: 24,
+    textAlign: 'center',
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 16.00,
+
+    elevation: 24,
+  },
+  addText: {
+    fontSize: 45,
+    fontWeight: 300,
+    color: '#C0C0C0',
+    marginBottom: 5,
+  }
 });
---
+++ README.md
@@ -0,0 +1,73 @@
+<h1 align="center"> Native Todo </h1>
+
+<p align="center">
+Organize your way and day, with Native Todo. <br/>
+</p>
+
+<p align="center">
+  <a href="#-technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-about">About</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-installation">Installation</a>
+</p>
+
+<p align="center">
+  <img src=".github/preview.jpg" width="100%">
+</p>
+
+## 🚀 Technologies
+
+This project was developed with the following technologies:
+
+- ``React Native`` & ``JavaScript``
+- ``Git`` & ``Github``
+-  ``Figma``
+
+## 📖 About 
+
+### Description
+A simple React Native app, where you can create custom tasks and delete them width a simple and friendly UI, easy to use.
+
+### Objective
+Learn and practice the following languages: React Native, JavaScript, and the design platform Figma. 
+
+## 🔖 Layout
+
+You can view the layout of the project through [in here](https://www.figma.com/file/PrBiS30nIdMFRhPAjfI9uz/Native-Todo?node-id=0%3A1&t=cyj9RvMbv0wZ5QXU-1). You must have an account at [Figma](https://figma.com) to access it.
+
+## 🛠 Installation
+
+```javascript
+# Clone this repo:
+git clone https://github.com/devgoncalo/native-todo
+
+# Entry in folder
+cd native-todo
+
+# Install deps with npm or yarn
+npm install | yarn
+
+# Launch the app on android with react-native or yarn
+react-native run-android | yarn android
+
+# Launch the app on ios with react-native or yarn
+react-native run-ios | yarn ios
+
+```
+
+## 📫 Contribute
+To contribute to Native Todo follow these steps:
+1. Fork this repository.
+2. Create a branch: `git checkout -b <branch_name>`.
+3. Make your changes and commit them: `git commit -m '<commit_message>'`
+4. Push to original branch: `git push origin <project_name> / <location>`
+5. Create the pull request.
+Alternatively, see the GitHub documentation at [how to create a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request ).
+
+## :memo: Licence
+
+This project is licensed under the MIT license.
+
+---
+
+Made with ♥ by [Gonçalo Pinto](https://github.com/devgoncalo)
